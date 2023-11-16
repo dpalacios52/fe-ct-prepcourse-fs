ff4bab12798aaafb74f10f0d4430391f4e73c5c6
dpalacios52 <dpalacios524@gmail.com>
2023-11-16 18:00:09 -0500

    mensaje

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,36 +3,50 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // Tu código: 
+   return string;
 }
+
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
-}
+      return x + y;
+   }
+   suma(8, 2);
+
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
-}
+      return x - y;
+   } 
+resta(6, 3);
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
-}
+      return x / y;
+   }
+   divide(8, 4);
+   
+
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y;
 }
+multiplica(5, 5);
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y;
 }
-
+obtenerResto(50, 6);
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    devolverString,
